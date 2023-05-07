c5192d21d33f5b96853123a1b51394bcb20fc7aa
Sören Brunk <soeren@brunk.io>
2023-05-07 17:59:49 +0200

    Rename tokenizers-scala module to tokenizers

--- scripts/src/scala/tokenizers/package.scala
+++ scripts/src/scala/tokenizers/package.scala
@@ -7,5 +7,14 @@ package object scripts {
   val nativeProject: model.CrossProjectName =
     model.CrossProjectName(model.ProjectName("native"), None)
   val mainProject: model.CrossProjectName =
-    model.CrossProjectName(model.ProjectName("tokenizers-scala"), None)
+    model.CrossProjectName(model.ProjectName("tokenizers"), None)
+
+  // will publish these with dependencies
+  def projectsToPublish(crossName: model.CrossProjectName): Boolean =
+    crossName.name.value match {
+      case "native" | "tokenizers" => true
+      case _ => false
+    }
+  
+  val groupId = "io.brunk.tokenizers"
 }
--- tokenizers/src/scala/io/brunk/tokenizers/Offset.scala
+++ tokenizers/src/scala/io/brunk/tokenizers/Offset.scala
@@ -2,5 +2,3 @@ package io.brunk.tokenizers
 
 private[tokenizers]
 class NativeOffsets(val starts: Array[Long], val ends: Array[Long])
-
-case class Offset(start: Long, end: Long)
